8bd659589e5c75de3cc435a63fe8ca9db12e5ac3
curious95 <kamalpradhan95@outlook.com>
2019-05-03 12:42:44 +0100

    data fetch done

--- src/main/java/com/dev/elastic/DataBaseFetcer.java
+++ src/main/java/com/dev/elastic/DataBaseFetcer.java
@@ -1,19 +1,39 @@
 package com.dev.elastic;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class DataBaseFetcer {
 
+	static Properties prop = new Properties();
 	// Connect to your database.
 	// Replace server name, username, and password with your credentials
 	public static void main(String[] args) {
-		String connectionUrl = "jdbc:sqlserver://yourserver.database.windows.net:1433;" + "database=AdventureWorks;"
-				+ "user=yourusername@yourserver;" + "password=yourpassword;" + "encrypt=true;"
-				+ "trustServerCertificate=false;" + "loginTimeout=30;";
+
+		try (InputStream input = new FileInputStream("../doc.properties")) {
+
+			// load a properties file
+			prop.load(input);
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		String connectionUrl = "jdbc:sqlserver://"+prop.getProperty("ur")+":1433;" + "database="+prop.getProperty("db")+";"
+				+ "user="+prop.getProperty("ha")+";" + "password="+prop.getProperty("sh")+";";
 
 		ResultSet resultSet = null;
 
@@ -21,7 +41,7 @@ public class DataBaseFetcer {
 				Statement statement = connection.createStatement();) {
 
 			// Create and execute a SELECT SQL statement.
-			String selectSql = "SELECT TOP 10 Title, FirstName, LastName from SalesLT.Customer";
+			String selectSql = "SELECT TOP 10 * from "+prop.getProperty("tb");
 			resultSet = statement.executeQuery(selectSql);
 
 			// Print results from select statement
